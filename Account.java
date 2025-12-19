a7cd2128cfc4fc01b31fb70f71a13ef7e5fecbd6
202400181-lakshanaAi <202400181@sigc.edu>
2025-12-19 14:15:33 +0530

    Add files via upload

---
+++ Account.java
@@ -0,0 +1,36 @@
+package model;
+
+public class Account {
+
+    private int accountNumber;
+    private double balance;
+
+    public Account(int accountNumber, double balance) {
+        this.accountNumber = accountNumber;
+        this.balance = balance;
+    }
+
+    public int getAccountNumber() {
+        return accountNumber;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void deposit(double amount) {
+        balance += amount;
+    }
+
+    public boolean withdraw(double amount) {
+        if (amount <= balance) {
+            balance -= amount;
+            return true;
+        }
+        return false;
+    }
+
+    public String toString() {
+        return accountNumber + "," + balance;
+    }
+}
